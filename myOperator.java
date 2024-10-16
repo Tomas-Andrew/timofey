2221b5b735b4f71f9a8685fed735b3986edd4d44
Tom <a@a.com>
2024-10-16 21:59:43 +0700

    16.10.2024 Задача №3_Массивы

---
+++ myOperator.java
@@ -0,0 +1,15 @@
+public class myOperator {
+    public static void main(String[] args) {
+        double x = 12.0;
+        double y = 18.0;
+        double z = 34.0;
+        double a = (x+y+z) / 3;
+        System.out.println("Среднее арифметическое" + " = " + a);// Среднее арифметическое
+        int b = (int) (a / 2);
+        double c = (a / 2);
+        System.out.println(b); // Округление в меньшую сторону путем изменения типа на int
+        System.out.println((int)c); // Сохранение типа double
+
+
+    }
+}
