7bceca6faab83f8bdad87801fb4c5693227bbda7
Tom <a@a.com>
2024-10-17 14:48:50 +0700

    17.10.2024 Задача №3_Массивы

--- myOperator.java
+++ myOperator.java
@@ -1,17 +1,23 @@
+import java.util.Scanner;
+
 public class myOperator {
     public static void main(String[] args) {
-        double x = 12.0;
-        double y = 18.0;
-        double z = 34.0;
-        double a = (x+y+z) / 3;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double x = scanner.nextDouble();
+        System.out.print("Введите второе число: ");
+        double y = scanner.nextDouble();
+        System.out.print("Введите третье число: ");
+        double z = scanner.nextDouble();
+
+        double a = (x + y + z) / 3;
         System.out.println("Среднее арифметическое" + " = " + a);// Среднее арифметическое
-        int b = (int) (a / 2);
-        double c = (a / 2);
-        System.out.println(b); // Округление в меньшую сторону путем изменения типа на int
-        System.out.println((int)c); // Сохранение типа double
-        if (c > 3) {
+
+        double b = Math.floor(a / 2);
+        System.out.println("Деление среднего арифметического на 2" + " = " + b);
+        if (b > 3) {
             System.out.println("Программа выполнена корректно");
         }
-
     }
 }
