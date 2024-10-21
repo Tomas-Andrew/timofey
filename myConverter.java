9f4c6a856678dc5b04b7625492eda89c8d92c1bc
Tom <a@a.com>
2024-10-21 13:07:24 +0700

    21.10.2024 Задача №6 Операторы

--- myConverter.java
+++ myConverter.java
@@ -6,7 +6,7 @@ public class myConverter {
 
         System.out.print("Выберите что переводить: 1 - масса, 2 - расстояние: ");
         int x = scanner.nextInt();
-        if (x==1){
+        if (x == 1) {
             System.out.print("Выберите единицу измерения: 1 - грамм, 2 - килограмм, 3 - унции, 4 - пуд: ");
             int y = scanner.nextInt();
             System.out.print("Введите число: ");
@@ -23,22 +23,22 @@ public class myConverter {
                 System.out.println("Ошибка");
             }
         }
-        if (x==2){
-        System.out.print("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут: ");
-        int y = scanner.nextInt();
-        System.out.print("Введите число: ");
-        double z = scanner.nextDouble();
-        if (y == 1) {
-            System.out.println(z + " метров");
-        } else if (y == 2) {
-            System.out.println((z * 0.000621) + " миль");
-        } else if (y == 3) {
-            System.out.println((z * 1.09) + " ярдов");
-        } else if (y == 4) {
-            System.out.println((z * 3.28) + " футов");
-        } else {
-            System.out.println("Ошибка");
+        if (x == 2) {
+            System.out.print("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут: ");
+            int y = scanner.nextInt();
+            System.out.print("Введите число: ");
+            double z = scanner.nextDouble();
+            if (y == 1) {
+                System.out.println(z + " метров");
+            } else if (y == 2) {
+                System.out.println((z * 0.000621) + " миль");
+            } else if (y == 3) {
+                System.out.println((z * 1.09) + " ярдов");
+            } else if (y == 4) {
+                System.out.println((z * 3.28) + " футов");
+            } else {
+                System.out.println("Ошибка");
+            }
         }
     }
-}
 }
