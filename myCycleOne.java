1f2f89450b0df0937c6f6d0c38bbd47d7ad7e997
Tom <a@a.com>
2024-10-21 18:13:03 +0700

    21.10.2024 Задача №7 Циклы

---
+++ myCycleOne.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class myCycleOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите длину массива: ");
+        int length = scanner.nextInt();
+        int[] array = new int[length];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int x = 10;
+        int y = 20;
+        int z = 30;
+        for (int element : array) {
+            if (element == x || element == y || element == z) {
+                System.out.println("Данное значение имеется в константах");
+            }
+        }
+    }
+}
